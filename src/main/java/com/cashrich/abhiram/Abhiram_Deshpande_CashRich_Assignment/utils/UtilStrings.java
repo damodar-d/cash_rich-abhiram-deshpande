32d4cdbd29eec3ec869d7934df1682aad44e4f7a
Peter <designing.developer111@gmail.com>
2024-05-16 18:21:01 +0530

    added: API Header constant and Failure String

--- src/main/java/com/cashrich/abhiram/Abhiram_Deshpande_CashRich_Assignment/utils/UtilStrings.java
+++ src/main/java/com/cashrich/abhiram/Abhiram_Deshpande_CashRich_Assignment/utils/UtilStrings.java
@@ -3,6 +3,8 @@ package com.cashrich.abhiram.Abhiram_Deshpande_CashRich_Assignment.utils;
 public class UtilStrings {
 
     public static String SUCCESS = "SUCCESS";
+    public static String FAIL = "FAILURE";
     public static String FETCH_URI ="https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC,ETHLTC";
     public static String API_KEY = "27ab17d1-215f-49e5-9ca4-afd48810c149";
+    public static String API_HEADER_NAME = "X-CMC_PRO_API_KEY";
 }
