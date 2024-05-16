1a8abc0125befaffa004a5c174f869dd720733ed
Peter <designing.developer111@gmail.com>
2024-05-16 18:21:59 +0530

    added: RestTemplate logic for fetching coin data in String

--- src/main/java/com/cashrich/abhiram/Abhiram_Deshpande_CashRich_Assignment/web_services/FetchCoinDataService.java
+++ src/main/java/com/cashrich/abhiram/Abhiram_Deshpande_CashRich_Assignment/web_services/FetchCoinDataService.java
@@ -1,14 +1,30 @@
 package com.cashrich.abhiram.Abhiram_Deshpande_CashRich_Assignment.web_services;
 
+import com.cashrich.abhiram.Abhiram_Deshpande_CashRich_Assignment.utils.UtilStrings;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class FetchCoinDataService {
 
-    @GetMapping("fetch-coin-data")
+
+    private RestTemplate restTemplate;
+    @GetMapping("/fetch-coin-data")
     public String fetchCoinData(){
+
         String result="";
+        restTemplate = new RestTemplate();
+        HttpHeaders apiHeader = new HttpHeaders();
+        apiHeader.set(UtilStrings.API_HEADER_NAME,UtilStrings.API_KEY);
+        HttpEntity<String> httpEntity = new HttpEntity<>(apiHeader);
+        result = restTemplate.exchange(UtilStrings.FETCH_URI, HttpMethod.GET,httpEntity,String.class).getBody();
 
 
         return result;
