33f23145f96b5d136ce77440978f3d6bf0bafb32
Peter <designing.developer111@gmail.com>
2024-05-31 15:38:26 +0530

    added: standardized API HEADER and VALUE names

--- src/main/java/com/cashrich/abhiram/Abhiram_Deshpande_CashRich_Assignment/utils/UtilStrings.java
+++ src/main/java/com/cashrich/abhiram/Abhiram_Deshpande_CashRich_Assignment/utils/UtilStrings.java
@@ -1,10 +1,15 @@
 package com.cashrich.abhiram.Abhiram_Deshpande_CashRich_Assignment.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UtilStrings {
 
     public static String SUCCESS = "SUCCESS";
     public static String FAIL = "FAILURE";
     public static String FETCH_URI ="https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC,ETHLTC";
     public static String API_KEY = "27ab17d1-215f-49e5-9ca4-afd48810c149";
-    public static String API_HEADER_NAME = "X-CMC_PRO_API_KEY";
+    public static String THIRD_PARTY_API_HEADER_NAME = "X-CMC_PRO_API_KEY";
+    public static String TRUSTED_API_ORIGIN_HEADER_NAME = "CASHRICH-TRUSTED-ORIGIN";
+    public static String TRUSTED_API_ORIGIN_HEADER_VALUE = "api-secured-origin";
 }
