f7b26849935df78ed29924d03f85661438162fe3
Peter <designing.developer111@gmail.com>
2024-06-19 20:29:11 +0530

    updated: TRUSTED_API_ORIGIN_HEADER_NAME

--- src/main/java/com/crypto_rates/utils/UtilStrings.java
+++ src/main/java/com/crypto_rates/utils/UtilStrings.java
@@ -9,6 +9,6 @@ public class UtilStrings {
     public static String FETCH_URI_PLAIN ="https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=";
     public static String API_KEY = "27ab17d1-215f-49e5-9ca4-afd48810c149";
     public static String THIRD_PARTY_API_HEADER_NAME = "X-CMC_PRO_API_KEY";
-    public static String TRUSTED_API_ORIGIN_HEADER_NAME = "CASHRICH-TRUSTED-ORIGIN";
+    public static String TRUSTED_API_ORIGIN_HEADER_NAME = "TRUSTED-ORIGIN";
     public static String TRUSTED_API_ORIGIN_HEADER_VALUE = "api-secured-origin";
 }
